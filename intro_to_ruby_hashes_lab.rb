193e246965630723e3861d9674b6aaacbbf1ac4a
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-22 15:01:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,6 +41,6 @@ def update_counting_hash(hash, key)
   if update_counting_hash[{hash[key]}]
     hash[key] += 1
   else
-    hash[key] = 1
+    update_counting_hash[{hash[key]}] = 1
   update_counting_hash[hash]
 end
